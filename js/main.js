bbae4d3a5052c85c0c3f9e4a0b3ef49a8fdd0c58
travis <t3patterson@gmail.com>
2018-04-12 19:05:48 -0500

    nice

---
+++ js/main.js
@@ -0,0 +1,9 @@
+alert('there is a supercutie sitting next to me!!1!')
+var someLargeNum = 2728 * 823
+
+console.log('a VERY big number.....')
+console.log(someLargeNum)
+
+document.querySelector('button').addEventListener('click', function(){
+  alert('you clicked a button good job')
+})
